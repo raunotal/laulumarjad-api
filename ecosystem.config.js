1a8206ca9e888e74e08c1eb836b5dbb11e3e0e40
Rauno Tali <rauno@pats.ee>
2022-11-23 21:25:21 +0200

    deploy failed, testing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       host: ["laulumarjad.ee"],
       ref: "origin/main",
       repo: "git@github.com:raunotal/laulumarjad-api.git",
-      ssh_options: ["ForwardAgent=yes"],
+      // ssh_options: ["ForwardAgent=yes"],
       path: "/data01/virt104534/deploy/laulumarjad-api",
       "post-deploy": "pm2 reload /data01/virt104534/deploy/laulumarjad-api/current/ecosystem.config.js"
     },
